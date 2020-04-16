432218bfa858d46b385ee6df8fbf5b1b57012ce0
ncooney94 <nick.cooney@gmail.com>
2020-04-16 16:10:30 +1000

    form styling and layout. created signify component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { FormGroup, TextField } from '@material-ui/core';
 import { KeyboardTimePicker, KeyboardDatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
-// import 'date-fns';
 import DateFnsUtils from '@date-io/date-fns';
-// import MomentUtils from '@date-io/moment';
 
+import Signify from './Signify';
 
 
 const Form: React.FC = () => {
@@ -23,7 +22,7 @@ const Form: React.FC = () => {
   return(
     <FormWrap>
       <Title>Form</Title>
-      <FormGroup>
+      <Content>
         <TextField
         id="outlined-search"
         label="Task/Assignment"
@@ -37,44 +36,36 @@ const Form: React.FC = () => {
           variant="outlined"
         />
 
-<MuiPickersUtilsProvider utils={DateFnsUtils}>
-        {/* <KeyboardDatePicker
-          disableToolbar
-          variant="inline"
-          format="MM/dd/yyyy"
-          margin="normal"
-          id="date-picker-inline"
-          label="Date picker inline"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        />
-        <KeyboardDatePicker
-          margin="normal"
-          id="date-picker-dialog"
-          label="Date picker dialog"
-          format="MM/dd/yyyy"
-          value={selectedDate}
-          onChange={handleDateChange}
-          KeyboardButtonProps={{
-            'aria-label': 'change date',
-          }}
-        /> */}
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <TimeWrap>
         <KeyboardTimePicker
           margin="normal"
           id="time-picker"
-          label="Time picker"
+          label="Start Time"
           value={selectedDate}
           onChange={handleDateChange}
           KeyboardButtonProps={{
             'aria-label': 'change time',
           }}
         />
+        <Hyph>
+          <h1>-</h1>
+        </Hyph>
+        <KeyboardTimePicker
+          margin="normal"
+          id="time-picker"
+          label="Finish Time"
+          value={selectedDate}
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            'aria-label': 'change time',
+          }}
+        />
+      </TimeWrap>
     </MuiPickersUtilsProvider>
         
-      </FormGroup>
+        <Signify />
+      </Content>
     </FormWrap>
   )
 };
@@ -83,10 +74,41 @@ const FormWrap = styled.div`
   background-color: #e2e2e2;
   width: 50%;
   height: 100%;
+
+`
+
+const Content = styled.div`
+  margin: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  div {
+    padding: 5px 0;
+  }
+`
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-around;
+  
+  > div {
+    margin: 0 5px;
+  }
 `
 
 const Title = styled.h1`
   text-align: center;
+`
+
+const Hyph = styled.div`
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    margin: 0;
+    font-weight: 300;
+  }
 `
 
 export default Form;
---
+++ src/components/Signify.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Signify: React.FC = () => {
+
+  return(
+    <h1>Signify</h1>
+  )
+};
+
+export default Signify;
