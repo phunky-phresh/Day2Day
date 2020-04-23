b95b383608e3c9e16677c837c12871730a665df9
ncooney94 <nick.cooney@gmail.com>
2020-04-23 19:22:38 +1000

    coded dragndrop

--- src/components/DragComp.tsx
+++ src/components/DragComp.tsx
@@ -1,33 +1,86 @@
-import React, { useEffect } from 'react';
-import { motion } from 'framer-motion';
+import React, { useEffect, useState, useRef } from 'react';
+import { motion, useMotionValue } from 'framer-motion';
+import { findIndex, Position } from './Drag-Index';
+import move from 'array-move';
 import styled from 'styled-components';
 
-interface Tasks {
-  title: string;
-  detail: string;
-  time: number;
-  color: string;
-}
-
-interface Props {
-  tasks: Tasks
-}
 
 
-const DragComp: React.FC<Props> = ({tasks}) => {
+const Item = ({ color, setPosition, moveItem, i }) => {
+  const [ isDragging, setDragging ] = useState(false);
+
+  const ref = useRef(null);
+
+  const dragOrginY = useMotionValue(0);
+
+  useEffect(() => {
+    setPosition( i, {
+      height: ref.current.offsetHeight,
+      top: ref.current.offsetTop
+    });
+  });
 
   return(
-   
-      <Drag
-        drag='y' 
-        dragConstraints={{top: 0, bottom: 100}}
-      >
-      <h4>{tasks.title}</h4>
-      <p>{tasks.detail}</p>
-      <p>{tasks.time}</p>
-      <p>{tasks.color}</p>
-      </Drag>
+    <motion.li
+      ref={ref}
+      initial={false}
+      animate={isDragging ? onTop : flat}
+      style={{ background: color, height: heights[color]}}
+      whileHover={{ scale: 1.03 }}
+      whileTap={{ scale: 1.12 }}
+      drag='y'
+      dragOriginY={dragOriginY}
+      dragConstraints={{ top: 0, bottom: 0}}
+      dragElastic={1}
+      onDragStart={() => setDragging(true)}
+      onDragEnd={() => setDragging(false)}
+      onDrag={(e, { point }) => moveItem(i, point.y)}
+      positionTransition={({ delta }) => {
+        if (isDragging){
+          dragOriginY.set(dragOriginY.get() + delta.y);
+        }
+
+        return !isDragging;
+      }}
+    >
+
+    </motion.li>
   );
+};
+
+
+const DragComp: React.FC = () => {
+
+  const [ colors, setColors ] = useState(initialColors);
+
+
+  const positions = useRef<Position[]>([]).current;
+  const setPosition = (i: number, offset: Position) => (positions[i] = offset);
+
+  const moveItem = (i: number, dragOffset: number) => {
+    const targetIndex = findIndex(i, dragOffset, positions);
+    if (targetIndex !== i) setColors(move(colors, i, targetIndex));
+  };
+  
+  return(
+   <ul>
+     {colors.map((color, i) => (
+       <Item 
+        key={color}
+        i={i}
+        color={color}
+        setPosition={setPosition}
+        moveItem={moveItem}
+       />
+     ))}
+   </ul>
+  );
+};
+
+const onTop = { zIndex: 1};
+const flat = {
+  zIndex: 0,
+  transition: { delay: 0.3 }
 };
 
 const Drag = styled(motion.div)`
@@ -37,5 +90,12 @@ const Drag = styled(motion.div)`
     margin: 0;
   }
 `
+const initialColors = ["#FF008C", "#D309E1", "#9C1AFF", "#7700FF"];
+const heights = {
+  "#FF008C": 60,
+  "#D309E1": 80,
+  "#9C1AFF": 40,
+  "#7700FF": 100
+};
 
 export default DragComp;
